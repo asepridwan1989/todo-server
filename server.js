6d0061b42bc3a58c506f4acc0d09157a781e009b
asepridwan1989 <aseprmail@gmail.com>
2018-05-21 20:39:59 +0700

    fix  procfile

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const morgan = require('morgan')
+// const morgan = require('morgan')
 const cors = require('cors')
 const app = express()
 const mongoose = require('mongoose')
